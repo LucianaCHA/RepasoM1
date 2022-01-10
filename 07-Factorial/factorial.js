ffe06deeca7e6eb0206f9a3fdad44299f4042bcb
LucianaCHA <76781967+LucianaCHA@users.noreply.github.com>
2022-01-10 07:08:57 -0300

    ex7 done

--- 07-Factorial/factorial.js
+++ 07-Factorial/factorial.js
@@ -8,4 +8,7 @@
  * @returns {Number} - deberia retornar un number.
  */
 
-function factorial(num) {}
+function factorial(num) {
+    return num < 0 ? 'No existe factoial de negativos': num === 0 || num === 1 ? 1 : num * factorial(num - 1);
+}
+console.log(factorial(6))
